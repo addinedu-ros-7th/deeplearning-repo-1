cdabff18ea9ab1ee30d6442126e9fdcde94c5bad
sydname <sydneycan9@gmail.com>
2024-12-05 19:02:08 +0900

    add gui files

---
+++ CASS_brain/GUI/CASS_ui.py
@@ -0,0 +1,154 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5 import uic
+from Modules import *
+import cv2
+import sys
+import socket
+
+form_class = uic.loadUiType("CASS_ui.ui")[0]
+
+ESP32_IP = "172.20.10.8"
+ESP32_PORT = 8080
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+class MainWindow(QMainWindow, form_class):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+        self.duration = 0
+
+        self.faceCam = Camera()
+        self.daemon = True
+        self.faceCam.update.connect(self.updateFaceCam)
+
+        self.legCam = Camera()
+        self.daemon = True
+        self.legCam.update.connect(self.updateLegCam)
+
+        self.athentified = False
+
+        self.ath_model = FaceRecognitionModel() # face athentification model
+        self.setUserImage()
+    
+        self.setLabelCams()
+        self.cameraOn()
+
+        self.btnSend.clicked.connect(self.sendData)
+        self.lineEdit.returnPressed.connect(self.sendData)
+    
+    def setLabelCams(self):
+        self.width, self.height = self.labelFace.width(), self.labelFace.height()
+        self.facePixmap = QPixmap(self.width, self.height)
+
+        self.w, self.h = self.labelLegCam.width(), self.labelLegCam.height()
+        self.legPixmap = QPixmap(self.w, self.h)
+
+    def msgMaker(self, msg):
+        self.msg_list = []
+        self.msg_list.append(msg)
+
+    def sendData(self):
+        # self.labelCam.setText("hello")
+        msg = self.lineEdit.getText()
+        self.msgMaker(msg)
+        client_socket.connect((ESP32_IP, ESP32_PORT))
+        client_socket.send(self.msg_list)
+
+    # def setUserImage(self):
+    #     self.path = "../../../test/data/face/my_img/soyoung.png"
+    #     self.name = "soyoung"
+
+    #     self.ath_model.set_user_image(self.path)
+    #     self.ath_model.set_known_user(self.ath_model.my_face_encoding, self.name)
+
+    def cameraOn(self):
+        '''
+        turn a camera on when driver authentification is success
+        '''
+        if not self.athentified:
+            self.start = time.time()
+            self.faceCam.start()
+            self.faceCam.isRunning = True
+            self.faceVideo = cv2.VideoCapture(0)
+            print("camera on")
+
+        elif self.athentified:
+            self.labelFace.hide()
+            print("authentification success")
+            self.legCam.start()
+            self.legCam.isRunning = True
+            self.legVideo = cv2.VideoCapture(2)
+            pass
+
+    def legCamOn(self):
+        self.labelFace.hide()
+        self.legCam.start()
+        self.legCam.isRunning = True
+        self.legVideo = cv2.VideoCapture(2)
+
+    def authentification(self, frame):
+        '''
+        authentify a driver face duration 3 secs
+        '''
+        self.face_locations, self.face_names = self.ath_model.face_athentication(frame)
+
+        if self.face_names[0] == self.name:
+            if self.athentified == False:
+                for (top, right, bottom, left), name in zip(self.face_locations, self.face_names):
+                    cv2.rectangle(frame, (left - 10, top - 10), (right + 10, bottom + 10), (200, 100, 5), 2)
+                    cv2.rectangle(frame, (left - 10, bottom - 25), (right + 10, bottom + 10), (200, 100, 5), cv2.FILLED)
+                    font = cv2.FONT_HERSHEY_DUPLEX
+                    cv2.putText(frame, name, (left + 6, bottom - 6), font, .5, (255, 255, 255), 1)
+            
+                if self.duration > 3 and self.duration < 4:
+                    self.athentified = True
+                    QMessageBox.warning(self, "Authentification ", f"{self.name} Driver Authentification Success.")
+                    self.msgMaker("run")
+
+    def updateFaceCam(self):
+        ret, face_frame = self.faceVideo.read()
+        
+        if ret:
+            frame = cv2.cvtColor(face_frame, cv2.COLOR_BGR2RGB)
+            frame = cv2.flip(frame, 1)
+            
+            self.end = time.time()
+            self.duration = self.end - self.start
+
+            # self.authentification(frame)
+            
+            # self.DrowsyDetection(frame)
+            
+            # if self.isDrowsy1 != self.isDrowsy2:
+                # self.send_Drowsy()
+                
+            h, w, c = frame.shape
+            qImg = QImage(frame, w, h, w*c, QImage.Format_RGB888)
+            self.facePixmap = self.facePixmap.fromImage(qImg)
+            self.facePixmap = self.facePixmap.scaled(self.width, self.height)
+            self.labelFace.setPixmap(self.facePixmap)
+
+    def updateLegCam(self):
+        ret, self.frame = self.legVideo.read()
+        
+        if ret:
+            frame = cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGB)
+
+            '''TODO: send output frame on model'''
+
+            h, w, c = frame.shape
+            qImg = QImage(frame, w, h, w*c, QImage.Format_RGB888)
+            self.legPixmap = self.legPixmap.fromImage(qImg)
+            self.legPixmap = self.legPixmap.scaled(self.w, self.h)
+            self.labelLegCam.setPixmap(self.legPixmap)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+
+    sys.exit(app.exec_())
+    client_socket.close()
---
+++ CASS_brain/GUI/Modules.py
@@ -0,0 +1,138 @@
+import os
+import time
+import timm
+import face_recognition
+import serial
+import torch
+import torch.nn as nn
+import numpy as np
+import socket
+from torchvision import  transforms
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from huggingface_hub import hf_hub_download
+from ultralytics import YOLO
+from supervision import Detections
+
+class Camera(QThread):
+    update = pyqtSignal()
+
+    def __init__(self, sec=0, parent=None):
+        super().__init__()
+        self.main = parent
+        self.running = True
+
+    def run(self):
+        while self.isRunning:
+            self.update.emit()
+            time.sleep(0.07)
+
+    def stop(self):
+        self.running = False
+
+class Arduino(QThread):
+    distance_signal = pyqtSignal(str)  # 거리를 전달할 시그널
+
+    def __init__(self, parent=None):
+        super().__init__()
+        self.main = parent
+        self.client_socket = None
+        self.esp32_ip = '192.168.199.119'  # ESP32의 IP 주소
+        self.esp32_port = 8080  # ESP32에서 설정한 포트
+
+    def run(self):
+        # 아두이노 시리얼 포트 열기
+        try:
+            self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            # self.client_socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1) 
+            self.client_socket.connect((self.esp32_ip, self.esp32_port))
+            time.sleep(1)  # 시리얼 연결 대기
+            print('ESP32 Connected')
+        except serial.SerialException as e:
+            print(f"Could not open serial port: {e}")
+ 
+        while True:
+            try:
+                self.client_socket.settimeout(0.01) 
+                data = self.client_socket.recv(1024).decode()
+                self.distance_signal.emit(data)  # 시그널로 데이터 전달
+                time.sleep(0.03)
+            except:
+                pass
+
+    def stop(self):
+        print('ESP32 Disconnected')
+        self.client_socket.close()  # 시리얼 포트 닫기
+
+
+class DrowseDetectionModel(nn.Module):
+    def __init__(self):        
+        super().__init__()
+        self.model = timm.create_model('resnet18', pretrained=True, num_classes=2)
+        # self.model = models.resnet50(weights='IMAGENET1K_V2')
+        self.set_model()
+        self.transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Resize((224, 224)),
+            transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]), 
+        ])
+
+    def set_model(self):
+        self.model.eval()
+
+    def get_state_dict(self, checkpoint_path):
+        path = os.path.join(checkpoint_path, 'checkpoints/best_model.pth')
+        self.model.load_state_dict(torch.load(path))
+
+    def forward(self, x):
+        x = self.transform(x)#.cuda()
+        x = x.unsqueeze(0)
+        x = self.model(x)
+        x = x.argmax(1).item()
+        return x
+    
+class DetectionModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        model_path = hf_hub_download(repo_id="arnabdhar/YOLOv8-Face-Detection", filename="model.pt")
+        self.FaceDetection = YOLO(model_path)
+
+    def forward(self, x):
+        output = self.FaceDetection(x, verbose=False)
+        results = Detections.from_ultralytics(output[0])
+        bbox = results.xyxy[0].astype(int) + np.array([-40, -60, 40, 10])
+        return bbox
+    
+class FaceRecognitionModel():
+    def __init__(self):
+        super().__init__()
+        self.known_face_encodings = []
+        self.known_face_names = []
+
+    def set_user_image(self, path):
+        self.my_image = face_recognition.load_image_file(path)
+        self.my_face_encoding = face_recognition.face_encodings(self.my_image)[0]
+
+    def set_known_user(self, encoding, name):
+        self.known_face_encodings.append(encoding)
+        self.known_face_names.append(name)
+
+    def face_athentication(self, frame):
+        face_locations = face_recognition.face_locations(frame, model="cnn")
+        face_encodings = face_recognition.face_encodings(frame, face_locations)
+
+        face_names = []
+        for face_encoding in face_encodings:
+            matches = face_recognition.compare_faces(self.known_face_encodings, face_encoding, tolerance=0.35)
+            name = "unknown"
+
+            if True in matches:
+                first_match_index = matches.index(True)
+                name = self.known_face_names[first_match_index]
+
+            face_names.append(name)
+            if len(face_locations) == 0:
+                face_locations = 0
+
+        return face_locations, face_names
