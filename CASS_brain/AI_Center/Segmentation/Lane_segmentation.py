ee51bf9c50e4bf93d6c0fbb02513851b46c19f92
sydname <sydneycan9@gmail.com>
2024-12-11 11:28:46 +0900

    add try except

--- CASS_brain/AI_Center/Segmentation/Lane_segmentation.py
+++ CASS_brain/AI_Center/Segmentation/Lane_segmentation.py
@@ -37,56 +37,57 @@ class LaneSegmentation(nn.Module):
     def forward(self, frame, direction='straight', mode='drive'):
         output = self.model(frame, verbose=False, device=0)
         frame = output[0].orig_img
-
-        cls = output[0].boxes.cls
-        img_size = output[0].masks.orig_shape
-
-        mid_ = int(img_size[1]/2)
-        bot_ = img_size[0]
-
-        roads = {'center':[], 'side':[]}
-        for i, xy in enumerate(output[0].masks.xy):
-            self.center.get_centroid(xy)
-            point = np.array([self.center.centroid_x, self.center.centroid_y], dtype=np.int32)
-            c_name = self.N2C[cls[i].item()]
-            if c_name=='center_road':
-                roads['center'].append(np.expand_dims(point, axis=0))
-
-            if mode=='left':
-                if c_name=='left_road':
-                    roads['side'].append(np.expand_dims(point, axis=0))
-            elif mode=='right':
-                if c_name=='right_road':
-                    roads['side'].append(np.expand_dims(point, axis=0))
         
+        try:
+            cls = output[0].boxes.cls
+            img_size = output[0].masks.orig_shape
 
-        start_point = (mid_, bot_)
+            mid_ = int(img_size[1]/2)
+            bot_ = img_size[0]
 
-        if mode == 'drive':
-            road = roads['center']
-            if len(road) == 0:
-                road = roads['side']
-        else:
-            road = roads['side']
-            if len(road) == 0:
-                road = roads['center']    
+            roads = {'center':[], 'side':[]}
+            for i, xy in enumerate(output[0].masks.xy):
+                self.center.get_centroid(xy)
+                point = np.array([self.center.centroid_x, self.center.centroid_y], dtype=np.int32)
+                c_name = self.N2C[str(int(cls[i].item()))]
+                if c_name=='center_road':
+                    roads['center'].append(np.expand_dims(point, axis=0))
 
-        road = np.concatenate(road, axis=0)
-        road = road[road[:, 0].argsort()]
-        if direction == 'left':
-            road = road[0]
-        elif direction == 'right':
-            road = road[-1]
-        else:
-            if len(road) > 2:
-                road = road[1:-1].mean(0).astype(np.int32)
+                if mode=='left':
+                    if c_name=='left_road':
+                        roads['side'].append(np.expand_dims(point, axis=0))
+                elif mode=='right':
+                    if c_name=='right_road':
+                        roads['side'].append(np.expand_dims(point, axis=0))
+            
+            start_point = (mid_, bot_)
+
+            if mode == 'drive':
+                road = roads['center']
+                if len(road) == 0:
+                    road = roads['side']
             else:
-                road = road[0]        
+                road = roads['side']
+                if len(road) == 0:
+                    road = roads['center']    
 
-        cv2.arrowedLine(frame, start_point, road,
-                            color=(0, 0, 0), 
-                            thickness=5, tipLength=0.1)
+            road = np.concatenate(road, axis=0)
+            road = road[road[:, 0].argsort()]
+            if direction == 'left':
+                road = road[0]
+            elif direction == 'right':
+                road = road[-1]
+            else:
+                if len(road) > 2:
+                    road = road[1:-1].mean(0).astype(np.int32)
+                else:
+                    road = road[0]        
 
-        slope = -(start_point[0] - road[0])/(start_point[1] - road[1])
-        slope = np.rad2deg(np.arctan(slope)) 
-        return slope
+            cv2.arrowedLine(frame, start_point, road,
+                                color=(0, 0, 0), 
+                                thickness=5, tipLength=0.1)
+            slope = -(start_point[0] - road[0])/(start_point[1] - road[1])
+            slope = np.rad2deg(np.arctan(slope)) 
+        except:
+            slope = 0
+        return slope, frame
