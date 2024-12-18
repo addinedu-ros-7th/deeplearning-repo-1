56a690953bb717015e7764104bd92405d815e7ed
yunminseop <001208yun@gmail.com>
2024-12-18 18:12:05 +0900

    zigzag and lock system added

--- CASS_leg/command_processor/command_processor.ino
+++ CASS_leg/command_processor/command_processor.ino
@@ -16,6 +16,7 @@ const int motor_L_reverse = 27;  // 왼쪽 역박향 IN4
 
 
 // 정지 및 직후진, L1~3, R1~3 모드 저장용 변수
+int select_num;
 int steering_mode;
 
 // 차량 기본 속도 세팅
@@ -39,7 +40,7 @@ Delta brake = {0, 0}; // Siren 전용 감속 변수
 bool Siren = false;
 bool prev_siren = true;
 
-// LED, 부저 millis() 세팅
+// LED millis() 세팅
 unsigned long LED_start_time = 0;
 const unsigned long LED_DELAY_TIME = 5000;
 
@@ -53,9 +54,16 @@ int Emergency_cnt = 0;
 // 주행상황 플래그 변수 (정지 및 직후진)
 bool StopFlag = true;
 bool BackFlag = false;
+bool isReady = false;
 
 // 속도 출력용 변수
 int show_speed = 0;
+
+// 차선 탐색 후 직진 변환용 변수
+int prev_steer_status;
+int curr_steer_status;
+bool SearchForLines = false;
+bool isRight = false;
 
 
 WiFiServer server(500);
@@ -84,8 +92,8 @@ bool LED_checkTimer()
 // 직진
 void GO_FORWARD()
 {
-  curr_left_speed = basic_left_speed + d.l_delta-brake.l_delta;
-  curr_right_speed = basic_right_speed + d.r_delta-brake.r_delta;
+  curr_left_speed = basic_left_speed + d.l_delta - brake.l_delta;
+  curr_right_speed = basic_right_speed + d.r_delta - brake.r_delta;
 
   analogWrite(motor_L_forward, curr_left_speed);
   analogWrite(motor_L_reverse, 0);
@@ -119,6 +127,30 @@ void STOP()
 
   analogWrite(motor_L_forward, curr_left_speed);
   analogWrite(motor_L_reverse, curr_left_speed);
+}
+
+// 차선 탐색용 지그재그 주행
+void ZIGZAG(bool isRight)
+{
+  curr_left_speed = basic_left_speed + d.l_delta;
+  curr_right_speed = basic_right_speed + d.r_delta;
+
+  if (isRight)
+  {
+    analogWrite(motor_R_forward, curr_right_speed);
+    analogWrite(motor_R_reverse, 0);
+
+    analogWrite(motor_L_forward, 0);
+    analogWrite(motor_L_reverse, curr_left_speed);
+  }
+  else
+  {
+    analogWrite(motor_R_forward, 0);
+    analogWrite(motor_R_reverse, curr_right_speed);
+
+    analogWrite(motor_L_forward, curr_left_speed);
+    analogWrite(motor_L_reverse, 0);
+  }
 }
 
 // 탑승자 무반응 비상상황
@@ -170,6 +202,27 @@ void Emergency()
   }
 }
 
+void SearchForLine()
+{
+  static int cnt = 0;
+  cnt ++;
+  if (cnt <= 60)
+  {
+    if (cnt >= 30)
+    {
+      ZIGZAG(true);
+    }
+    else
+    {
+      ZIGZAG(false);
+    }
+  }
+  else
+  {
+    cnt = 0;
+  }
+}
+
 void setup() {
   Serial.begin(115200);  // 시리얼 모니터 시작
 
@@ -216,173 +269,108 @@ void loop() {
         Serial.println("클라이언트 연결 성공\n");
         break;
       
-      case 21: // 차주 인식
+      case 21: // 차주 인식 + 동작 가능
         client.print("welcome soyoung\n");
+        select_num = 0;
+        isReady = true;
+        StopFlag = true;
         BLUE_ON = true;
         LED_startTimer(); 
         break;
 
+      case 22: // 운행 종료
+        if (StopFlag)
+        {
+          client.print("Have a nice day.\n");
+          select_num = 0;
+          isReady = false;
+          // StopFlag = true;
+          BLUE_ON = true;
+          LED_startTimer();
+        }
+        break;
+
       case 31: // 직진
-          BackFlag = false;
-          StopFlag = false;
-          steering_mode = 1;
+        BackFlag = false;
+        StopFlag = false;
+        SearchForLines = false;
+        select_num = 1;
         break;
 
       case 32: // 정지
-        steering_mode = 0;
+        select_num = 0;
+        SearchForLines = false;
         BackFlag = false;
         StopFlag = true;
         break;
 
       case 33: // 후진
         BackFlag = true;
-        steering_mode = 1;
+        select_num = 1;
         StopFlag = false;
+        SearchForLines = false;
         break;
 
-      case 34:
-        steering_mode = 8;
+      case 34: // 가속
+        select_num = 8;
         StopFlag = false;
+        SearchForLines = false;
         break;
       
       case 41: // L1
         StopFlag = false;
-        steering_mode = 2;    
+        select_num = 2;
+        SearchForLines = false;    
         break;
       
       case 42: // L2
         StopFlag = false;
-        steering_mode = 3;
-
+        SearchForLines = false;
+        select_num = 3;
         break;
 
       case 43: // L3
         StopFlag = false;
-        steering_mode = 4;
+        SearchForLines = false;
+        select_num = 4;
         break;
 
       case 51: // R1
         StopFlag = false;
-        steering_mode = 5;
+        SearchForLines = false;
+        select_num = 5;
         break;
 
       case 52: // R2
         StopFlag = false;
-        steering_mode = 6;
+        SearchForLines = false;
+        select_num = 6;
         break;
       
       case 53: // R3
         StopFlag = false;
-        steering_mode = 7;        
+        SearchForLines = false;
+        select_num = 7;        
         break;
 
-      case 88: // 갓길 주차
-        Siren = true;
-        break;
-
-      case 99: // 비상 제동
+      case 77: // 차선 탐색
         StopFlag = false;
+        select_num = 9;
+        break;
+
+      case 88: // 응급상황 갓길주차
+        Siren = true;
+        SearchForLines = false;
+        break;
+
+      case 99: // 졸음 운전 대응
+        StopFlag = false;
+        SearchForLines = false;
         Emergency_Signal = true;
         break;
     }
 
-    // 명령별 조향모드 설정 (기본 가감속 구조체 변수 'd' 사용)
-    switch (steering_mode) {
-      case 0: // 정지
-        d = {-basic_left_speed, -basic_right_speed};
-
-      case 1: // 직진
-        d = {0, 0};
-        break;
-
-      case 2: // L1
-        d = {-40, 90};
-        break;
-
-      case 3: // L2
-        d = {-45, 110};
-        break;
-
-      case 4: // L3
-        d = {-50, 155};
-        break;
-      
-      case 5: // R1
-        d = {60, -20};
-        break;
-
-      case 6: // R2
-        d = {85, -20};
-        break;
-
-      case 7: // R3
-        d = {115, -20};
-        break;
-
-      case 8: // ACCEL
-        d = {40, 40};
-        break;
-    }
-
-    if(Siren && prev_siren) // 응급상황 감속
-    {
-      if (steering_mode > 0) // 정지 제외 나머지 모드에서만 작동
-      {
-        // Siren 전용 감속 구조체 변수 'brake' 사용
-        brake.l_delta+=1;
-        brake.r_delta+=1;
-        delay(15);
-        if (StopFlag)
-        {
-          STOP();
-        }
-        else
-        {
-          if(BackFlag)
-          {
-            GO_BACK();
-          }
-          else
-          {
-            GO_FORWARD();
-          }
-        }
-      }
-      if (curr_right_speed <= 0 || curr_left_speed <= 0) // 감속 중에 현재속도 음수될 시 정차
-      {
-        d = {-basic_left_speed, -basic_right_speed};
-        brake = {0, 0};
-        StopFlag = true;
-        Siren = false;
-        prev_siren = false;
-        STOP();
-      }
-
-    } 
-    else // 응급상황 아닐 시 정상주행
-    {
-      prev_siren = true;
-      if (StopFlag) // 정지 플래그 true - 정지
-      {
-        STOP();
-        digitalWrite(RED1, HIGH);
-        digitalWrite(RED2, HIGH);
-      }
-      else // 정지 플래그 false - 직진 혹은 후진
-      {
-        digitalWrite(RED1, LOW);
-        digitalWrite(RED2, LOW);
-        if (BackFlag) // 후진 플래그 true - 후진
-        {
-          GO_BACK();
-        }
-        else // 후진 플래그 false - 직진
-        {
-          GO_FORWARD();
-        }
-        
-      } 
-    }
+    prev_steer_status = curr_steer_status;
 
     //LED 작동함수
     if (BLUE_ON && LED_checkTimer())
@@ -395,16 +383,149 @@ void loop() {
       digitalWrite(BLUE1, LOW);
     }
 
+    // Serial.print("select_num: ");
+    // Serial.println(select_num);
+    // Serial.print("steering_mode: ");
+    // Serial.println(steering_mode);
+    
+    if (isReady)
+    {
+      steering_mode = select_num;
+      // 명령별 조향모드 설정 (기본 가감속 구조체 변수 'd' 사용)
+      switch (steering_mode) {
+        case 0: // 정지
+          d = {-basic_left_speed, -basic_right_speed};
 
-    //비상 제동
-    if (Emergency_Signal)
-    {
-      Emergency();
-    }
-    else
-    {
-      Emergency_Signal = false;
-      continue;
+        case 1: // 직진
+          d = {0, 0};
+          break;
+
+        case 2: // L1
+          d = {-40, 90};
+          break;
+
+        case 3: // L2
+          d = {-45, 110};
+          break;
+
+        case 4: // L3
+          d = {-50, 155};
+          break;
+        
+        case 5: // R1
+          d = {60, -20};
+          break;
+
+        case 6: // R2
+          d = {85, -20};
+          break;
+
+        case 7: // R3
+          d = {115, -20};
+          break;
+
+        case 8: // ACCEL
+          d = {40, 40};
+          break;
+
+        case 9: // 차선 탐색
+          d = {20, 40};
+          SearchForLines = true;
+          break;
+      }
+
+      curr_steer_status = steering_mode;
+
+      if (curr_steer_status != 9 && prev_steer_status == 9)
+      {
+        BackFlag = false;
+      }
+
+      if(Siren && prev_siren) // 응급상황 감속
+      {
+        if (steering_mode > 0) // 정지 제외 나머지 모드에서만 작동
+        {
+          // Siren 전용 감속 구조체 변수 'brake' 사용
+          brake.l_delta+=1;
+          brake.r_delta+=1;
+          delay(15);
+          if (StopFlag)
+          {
+            STOP();
+          }
+          else
+          {
+            if(SearchForLines)
+            {
+              SearchForLine();
+            }
+            else
+            {
+              if(BackFlag)
+              {
+                GO_BACK();
+              }
+              else
+              {
+                GO_FORWARD();
+              }
+            }
+          }
+        }
+        if (curr_right_speed <= 0 || curr_left_speed <= 0) // 감속 중에 현재속도 음수될 시 정차
+        {
+          d = {-basic_left_speed, -basic_right_speed};
+          brake = {0, 0};
+          StopFlag = true;
+          Siren = false;
+          prev_siren = false;
+          STOP();
+        }
+
+      } 
+      else // 응급상황 아닐 시 정상주행
+      {
+        prev_siren = true;
+        if (StopFlag) // 정지 플래그 true - 정지
+        {
+          SearchForLines = false;
+          STOP();
+          digitalWrite(RED1, HIGH);
+          digitalWrite(RED2, HIGH);
+        }
+        else // 정지 플래그 false - 직진 혹은 후진
+        {
+          digitalWrite(RED1, LOW);
+          digitalWrite(RED2, LOW);
+          if(SearchForLines)
+          {
+            SearchForLine();
+          }
+          else
+          {
+            SearchForLines = false;
+            if (BackFlag) // 후진 플래그 true - 후진
+            {
+              GO_BACK();
+            }
+            else // 후진 플래그 false - 직진
+            {
+              GO_FORWARD();
+            }
+          }
+        } 
+      }
+
+      //비상 제동
+      if (Emergency_Signal)
+      {
+        Emergency();
+      }
+      else
+      {
+        Emergency_Signal = false;
+        continue;
+      }
     }
   }
 }
